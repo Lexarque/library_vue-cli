a6def3a541a1ca50d65bea8ed68c569c1c9e51e4
Lexarque <ignishalcyon1@gmail.com>
2022-03-22 11:59:50 +0700

    "new commit"

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
 import { createApp } from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 
 import {createWebHistory, createRouter} from 'vue-router'
 import Home from './components/Home.vue'
 import Profile from './components/Profile.vue'
+import Students from './components/Students.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', name:'Home', component: Home},
-        {path: '/profile', name:'Profile', component: Profile}
+        {path: '/profile', name:'Profile', component: Profile},
+        {path: '/students', name:'Students', component: Students}
     ],
     base: '/'
 })
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.config.globalProperties.axios = axios
+app.mount('#app')
